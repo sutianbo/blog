1bc084ed03522f380d3ce82f8b1b57ad667abb42
sutianbo <sutianbo@zy.com>
2017-11-21 16:34:29 +0800

    first commit

---
+++ web/src/main/java/com/blog/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@RequestMapping(value = "/index",method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+}
