fd7591078c5d6dfc7324dddea9813b48095737ba
sutianbo <378437525@qq.com>
2021-08-04 10:20:52 +0800

    Update IndexController.java

--- web/src/main/java/com/blog/web/IndexController.java
+++ web/src/main/java/com/blog/web/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 
 	@RequestMapping(value = "/index",method = RequestMethod.GET)
 	public String index() {
-		String s = "1";
+		String s = "2";
 		return "index";
 	}
 
